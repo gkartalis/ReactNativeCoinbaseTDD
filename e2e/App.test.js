dcb56fc0cdaeee20e6ed2fd73a710a2aca106dfa
George Kartalis <g.kartalis7@gmail.com>
2020-01-08 23:50:50 +0200

    added app e2e tests

--- e2e/App.test.js
+++ e2e/App.test.js
@@ -14,4 +14,9 @@ describe('<App />', () => {
   it('the modal should not be visible', async () => {
     await expect(element(by.id('CurrencyDetailsModal'))).toBeNotVisible();
   });
+
+  it.only('should let user scroll through the list', async () => {
+    await element(by.id('DisplayCurrencies')).swipe("up", "slow", 0.6);
+    await element(by.id('DisplayCurrencies')).swipe("down", "slow", 0.6);
+  });
 });
