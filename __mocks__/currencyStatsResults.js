d83f698bd043fab6615ad768474e3da6d7ab9596
George Kartalis <g.kartalis7@gmail.com>
2020-01-08 12:58:32 +0200

    added mocked responses

---
+++ __mocks__/currencyStatsResults.js
@@ -0,0 +1,8 @@
+export const currencyStatsResults = {
+    open: 7055.00000000,
+    high: 7590.00000000,
+    low: 6950.10000000,
+    volume: 3429.69498856,
+    last: 7519.00000000,
+    volume_30day: 36002.80642838
+};
---
+++ __mocks__/errorStubbedResults.js
@@ -0,0 +1,3 @@
+export const error = {
+  message: 'Something went wrong..!'
+};
