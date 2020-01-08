8cc41fedd94e4e405149520806a0745d084635ad
George Kartalis <g.kartalis7@gmail.com>
2020-01-08 14:20:34 +0200

    added custom hook to app

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
-  View,
-  Text
 } from 'react-native';
 import {DisplayCurrencies} from './src/components/DisplayCurrencies';
+import useDataFetching from './src/customHooks/useDataFetching';
+import {API_URL} from './src/constants/api';
 
 const App = () => {
+  const { data, isLoading, errorMessage } = useDataFetching(`${API_URL}/products`);
   const [showModal, toggleModal] = useState(false);
   return (
     <>
       <SafeAreaView testID="App" style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <DisplayCurrencies data={[]} isLoading={true} showModal={showModal} toggleModal={toggleModal} />
+        <DisplayCurrencies
+          errorMessage={errorMessage}
+          data={data}
+          isLoading={isLoading}
+          showModal={showModal}
+          toggleModal={toggleModal}
+        />
       </SafeAreaView>
     </>
   );
